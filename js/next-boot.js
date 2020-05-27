0fd33b68e19e535e6673c03d79ee0ed1313f5443
sarwang <sarwang@tutorabc.com.cn>
2020-05-27 18:53:08 +0800

    Site updated: 2020-05-27 18:53:07

--- js/next-boot.js
+++ js/next-boot.js
@@ -12,7 +12,14 @@ NexT.boot.registerEvents = function() {
     event.currentTarget.classList.toggle('toggle-close');
     var siteNav = document.querySelector('.site-nav');
     var animateAction = siteNav.classList.contains('site-nav-on') ? 'slideUp' : 'slideDown';
-
+    $(document).ready(function(){
+      $(document).on('click', '.fold_hider', function(){
+        $('>.fold', this.parentNode).slideToggle();
+        $('>:first', this).toggleClass('open');
+      });
+      //默认情况下折叠
+      $("div.fold").css("display","none");
+    });
     if (typeof Velocity === 'function') {
       Velocity(siteNav, animateAction, {
         duration: 200,
